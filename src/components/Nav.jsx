203eeec08ee093a287125334be621909733d14fb
Amina <minassouane@gmail.com>
2026-01-08 22:54:07 +0100

    Add navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export function Nav() {
+  return (
+    <header>
+      <nav>
+        <button class="home">Home</button>
+        <button class="shop">Shop</button>
+        <button class="cart">Cart</button>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Store.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import { Nav } from "./Nav.jsx";
+
+export function Store() {
+  return <Nav />;
+}
