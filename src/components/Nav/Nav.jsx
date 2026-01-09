d1a67c6c79eaa860ce8f931b0d8f7b51e3aaa00f
Amina <minassouane@gmail.com>
2026-01-09 05:24:32 +0100

    Add CSS modules

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,13 @@
+import styles from "./Nav.module.css";
+
 export function Nav() {
   return (
-    <header>
-      <nav>
-        <button class="home">Home</button>
-        <button class="shop">Shop</button>
-        <button class="cart">Cart</button>
+    <header className={styles.header}>
+      <h1 className={styles.title}>Amina's Store</h1>
+      <nav className={styles.nav}>
+        <button className={styles.button}>Home</button>
+        <button className={styles.button}>Shop</button>
+        <button className={styles.button}>Cart</button>
       </nav>
     </header>
   );
