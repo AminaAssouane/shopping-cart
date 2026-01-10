7039fe2165a5fe9d7808b1bead9ed2af645eb054
Amina <minassouane@gmail.com>
2026-01-10 15:16:35 +0100

    Import react router

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from "react";
 import { Nav } from "../Nav/Nav.jsx";
+import { Home } from "../Home/Home.jsx";
+import { Shop } from "../Shop/Shop.jsx";
+import { Cart } from "../Cart/Cart.jsx";
 
 export function Store() {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Home />
+      <Shop />
+      <Cart />
+    </>
+  );
 }
